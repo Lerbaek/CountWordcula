858f37dfe3aad59c6c413ae174340e94ecd7e3ae
Kristoffer Lerbæk <github@lerbaek.dk>
2022-11-16 21:59:41 +0100

    Create CountWordsTests class with empty test

---
+++ CountWordcula.Test/CountWordsTests.cs
@@ -0,0 +1,17 @@
+﻿using CountWordcula.Read;
+using Xunit;
+
+namespace CountWordcula.Test
+{
+  public class CountWordsTests
+  {
+    private readonly ICountWords uut;
+
+    public CountWordsTests(ICountWords uut) => this.uut = uut;
+
+    [Fact]
+    public void Run_WordsAreCounted_OutputFilesContainCorrectData()
+    {
+    }
+  }
+}
