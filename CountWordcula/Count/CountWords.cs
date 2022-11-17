266373059c358d729d923bd94ebd4bbb99a9c692
Kristoffer Lerbæk <github@lerbaek.dk>
2022-11-17 20:44:32 +0100

    Refactor CountWords to have all string configurations as organized constants for easy overview

--- CountWordcula/Count/CountWords.cs
+++ CountWordcula/Count/CountWords.cs
@@ -1,5 +1,5 @@
 ﻿using CountWordcula.Validate;
-﻿using GoCommando;
+using GoCommando;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
@@ -9,8 +9,51 @@ namespace CountWordcula.Count;
 [Description("Read data from multiple files in a given directory.")]
 public class CountWords : ICountWords
 {
-  [Parameter("input-path", "i")]
-  [Description("Where are the input files?")]
+  #region Names
+
+  private const string InputPathName = "input-path";
+  private const string InputPathShortName = "i";
+
+  private const string OutputPathName = "output-path";
+  private const string OutputPathShortName = "o";
+
+  private const string ExtensionName = "extension";
+  private const string ExtensionShortName = "e";
+
+  private const string SegregateName = "segregate";
+  private const string SegregateShortName = "s";
+
+  private const string ForceName = "force";
+  private const string ForceShortName = "f";
+
+  #endregion
+
+  #region Descriptions
+
+  private const string InputPathDescription  = "Where are the input files?";
+  private const string OutputPathDescription = "Where should the output files be placed?";
+  private const string ExtensionDescription  = "Which file extension does the input file(s) have?";
+  private const string SegregateDescription  = "Should there be separate output files per input file?";
+  private const string ForceDescription      = "Should any existing files be overwritten?";
+
+  #endregion
+
+  #region Optional
+
+  private const bool ExtensionOptional = true;
+  private const bool SegregateOptional = true;
+  private const bool ForceOptional     = true;
+
+  #endregion
+
+  #region Default values
+
+  private const string ExtensionDefaultValue = "txt";
+
+  #endregion
+
+  private static readonly IServiceProvider Provider;
+
   static CountWords()
   {
     Provider = new ServiceCollection()
@@ -23,23 +66,26 @@ public class CountWords : ICountWords
       .AddSingleton<CountWordsValidator>()
       .BuildServiceProvider();
   }
+
+  [Parameter(InputPathName, InputPathShortName)]
+  [Description(InputPathDescription)]
   public string InputPath { get; set; } = null!;
 
-  [Parameter("output-path", "o")]
-  [Description("Where should the output files be placed?")]
+  [Parameter(OutputPathName, OutputPathShortName)]
+  [Description(OutputPathDescription)]
   public string OutputPath { get; set; } = null!;
 
-  [Parameter("extension", "e", true, "txt")]
-  [Description("Where should the output files be placed?")]
-  public string Extension { get; set; } = null!;
+  [Parameter(ExtensionName, ExtensionShortName, ExtensionOptional, ExtensionDefaultValue)]
+  [Description(ExtensionDescription)]
+  public string Extension { get; set; } = ExtensionDefaultValue;
 
 
-  [Parameter("segregate", "s", true)]
-  [Description("Should there be separate output files per input file?")]
+  [Parameter(SegregateName, SegregateShortName, SegregateOptional)]
+  [Description(SegregateDescription)]
   public bool Segregate { get; set; }
 
-  [Parameter("force", "f", true)]
-  [Description("Should any existing files be overwritten?")]
+  [Parameter(ForceName, ForceShortName, ForceOptional)]
+  [Description(ForceDescription)]
   public bool Force { get; set; }
 
   public void Run()
