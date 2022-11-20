2915b6207e27bf6a4c011c560e2c861e12754f98
Kristoffer Lerbæk <github@lerbaek.dk>
2022-11-20 20:13:14 +0100

    Add memory efficient file reader implementations

---
+++ CountWordcula.Backend/FileReader/MemoryEfficientFileReader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+
+namespace CountWordcula.Backend.FileReader;
+
+public class MemoryEfficientFileReader : IFileReader
+{
+  private readonly ConcurrentDictionary<string, long> wordCount;
+
+  public MemoryEfficientFileReader() => wordCount = new ConcurrentDictionary<string, long>();
+
+  public Task<IDictionary<string, long>> GetWordCountAsync(string fileName)
+  {
+    using var reader = File.OpenText(fileName);
+    var word = string.Empty;
+    while (reader.Peek() >= 0)
+    {
+      var character = (char)reader.Read();
+      if (character is '.' or ',')
+        continue;
+      if (character == ' ')
+      {
+        wordCount[word] = wordCount.ContainsKey(word) ? wordCount[word] + 1 : 1;
+        word = string.Empty;
+      }
+      else
+        word += character;
+    }
+
+    return Task.FromResult<IDictionary<string, long>>(wordCount);
+  }
+}
---
+++ CountWordcula.Backend/FileReader/MemoryEfficientParallelFileReader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Concurrent;
+
+namespace CountWordcula.Backend.FileReader;
+
+public class MemoryEfficientParallelFileReader : IFileReader
+{
+  private readonly ConcurrentDictionary<string, long> wordCount;
+
+  public MemoryEfficientParallelFileReader() => wordCount = new ConcurrentDictionary<string, long>();
+
+  public async Task<IDictionary<string, long>> GetWordCountAsync(string fileName)
+  {
+    using var reader = File.OpenText(fileName);
+    var word = string.Empty;
+    var tasks = new List<Task>();
+    while (reader.Peek() >= 0)
+    {
+      var line = await reader.ReadLineAsync();
+      tasks.Add(Task.Run(
+        () =>
+        {
+          foreach (var word in line!.Split()
+                     .Select(w => w.TrimEnd(',', '.'))
+                     .Where(w => !string.IsNullOrWhiteSpace(w)))
+          {
+            wordCount[word] = wordCount.ContainsKey(word)
+              ? wordCount[word] + 1
+              : 1;
+          }
+        }));
+    }
+
+    await Task.WhenAll(tasks);
+    return wordCount;
+  }
+}
