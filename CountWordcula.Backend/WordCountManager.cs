3e943d881b6be5c244828ae6c537955f8d127043
Kristoffer Lerbæk <github@lerbaek.dk>
2022-11-23 00:17:25 +0100

    Add first-layer logging

--- CountWordcula.Backend/WordCountManager.cs
+++ CountWordcula.Backend/WordCountManager.cs
@@ -1,6 +1,8 @@
 ﻿using CountWordcula.Backend.FileRead;
 using CountWordcula.Backend.FileWrite;
+using CountWordcula.Backend.Registry;
 using CountWordcula.Backend.Validate;
+using Microsoft.Extensions.Logging;
 using static CountWordcula.Backend.Registry.ConfigurationRegistry;
 
 namespace CountWordcula.Backend;
@@ -11,17 +13,20 @@ public class WordCountManager : IWordCountManager
   private readonly IFileWriter fileWriter;
   private readonly WordCountConfigurationValidator wordCountConfigurationValidator;
   private readonly ExcludeFileValidator excludeFileValidator;
+  private readonly ILogger<WordCountManager> logger;
 
   public WordCountManager(
     IFileReader fileReader,
     IFileWriter fileWriter,
     WordCountConfigurationValidator wordCountConfigurationValidator,
-    ExcludeFileValidator excludeFileValidator)
+    ExcludeFileValidator excludeFileValidator,
+    ILogger<WordCountManager> logger)
   {
     this.fileReader = fileReader;
     this.fileWriter = fileWriter;
     this.wordCountConfigurationValidator = wordCountConfigurationValidator;
     this.excludeFileValidator = excludeFileValidator;
+    this.logger = logger;
   }
 
   public async Task<bool> RunAsync(string inputPath, string inputExtension, string outputPath, bool force = false) =>
@@ -34,15 +39,27 @@ public class WordCountManager : IWordCountManager
 
   public async Task<bool> RunAsync(WordCountConfiguration configuration)
   {
+    logger.LogInformation("Reading excluded words.");
     var excludedWords = await GetExcludedWordsAsync(configuration.InputPath!);
+    logger.LogDebug("{Count} lines of excluded words found.", excludedWords.Length);
     if (!await Validate(configuration, excludedWords))
       return false;
     await ExecuteAsync(configuration, excludedWords);
     return true;
   }
 
-  private async Task<bool> Validate(WordCountConfiguration configuration, string[] excludedWords) =>
-    await ValidateInput(configuration) && await ValidateExcludedWords(excludedWords);
+  private async Task<bool> Validate(WordCountConfiguration configuration, string[] excludedWords)
+  {
+    logger.LogInformation("Validating input configuration.");
+    var inputValid = await ValidateInput(configuration);
+    logger.LogDebug("Input configuration is {Validity}.", (inputValid ? "" : "not ") + "valid");
+
+    logger.LogInformation("Validating excluded words.");
+    var excludedWordsValid = await ValidateExcludedWords(excludedWords);
+    logger.LogDebug("Excluded words are {Validity}.", (excludedWordsValid ? "" : "not ") + "valid");
+
+    return inputValid && excludedWordsValid;
+  }
 
   private async Task<bool> ValidateInput(WordCountConfiguration configuration) =>
     (await wordCountConfigurationValidator.ValidateAsync(configuration)).IsValid;
@@ -54,16 +71,40 @@ public class WordCountManager : IWordCountManager
     WordCountConfiguration configuration,
     string[]? excludedWords = null)
   {
+    logger.LogInformation(
+      "Reading input files with extension {Extension} from {InputPath}",
+      $".{configuration.InputExtension}",
+      configuration.InputPath);
+
     var inputFileNames = Directory
       .GetFiles(configuration.InputPath!)
-      .Where(fileName => fileName.EndsWith(configuration.InputExtension!) && !fileName.EndsWith(InputExcludeFileName));
+      .Where(fileName => fileName.EndsWith(configuration.InputExtension!) && !fileName.EndsWith(InputExcludeFileName))
+      .ToArray();
 
-    excludedWords ??= await GetExcludedWordsAsync(configuration.InputPath!);
+    logger.LogInformation("{Count} input files found.", inputFileNames.Length);
+    foreach (var fileName in inputFileNames.Select(Path.GetFileName))
+      logger.LogDebug("{FileName}", fileName);
+
+    if (excludedWords is null)
+    {
+      logger.LogInformation(
+        "No excluded words were provided. Reading from {ExcludedFileName} instead.",
+        InputExcludeFileName);
+
+      excludedWords = await GetExcludedWordsAsync(configuration.InputPath!);
+    }
+
+    logger.LogInformation("Counting words ...");
     var wordCountTasks = inputFileNames.Select(fileName => fileReader.GetWordCountAsync(fileName, excludedWords));
     var wordCounts = await Task.WhenAll(wordCountTasks);
 
+    logger.LogInformation("Done!");
+
     var wordCount = WordCount.Combine(wordCounts);
+
+    logger.LogInformation("Writing word counts to per-letter files in {OutputPath}", configuration.OutputPath);
     await fileWriter.WriteOutputFilesAsync(configuration.OutputPath!, wordCount);
+    logger.LogInformation("All done. See output files in {AbsoluteOutputPath}", Path.GetFullPath(configuration.OutputPath!));
   }
 
   private static async Task<string[]> GetExcludedWordsAsync(string inputPath)
